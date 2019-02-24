915f9b0299e3d3ee15791358c313d050e7404683
esekyere <ericsekyere1@gmail.com>
2019-02-24 18:38:46 -0500

    Minor Updates
    UI Fixes
    Adding styles
    Change theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,21 +14,31 @@ export class AppComponent implements OnInit {
   perms;
   showResult = false;
   baseURL = environment.domain;
-  constructor(private http: HttpService) {}
+  breakpoint: number;
+  constructor(private http: HttpService) { }
 
   private fetchData(url) {
-   return this.http.fetchData(url);
+    return this.http.fetchData(url);
   }
 
   make_request() {
     /*console.log(`${this.baseURL}/data?let=${this.letters}`);
     this.fetchData(`${this.baseURL}/data?let=${this.letters}`)
     .subscribe(data => this.perms = data);*/
-    this.perms = { '3': ['ott', 'tot', 'toy', 'tty', 'tyt', 'yot'], '4': ['toty', 'tyto'] }
+    this.perms = {
+      '3': ['ert', 'eru', 'ery', 'etr', 'ety',
+      'eur', 'eyr', 'ret', 'rey', 'rte', 'rtu',
+      'rue', 'rut', 'ruy', 'rye', 'ter', 'try', 'tue', 'tur', 'tuy', 'tye', 'tyr', 'ure', 'ury', 'ute', 'utr', 'yer', 'yet'],
+      '4': ['trey', 'tyre', 'uret', 'urey', 'yurt'], '5': ['tuyer'] } ;
     this.showResult = true;
   }
 
   ngOnInit(): void {
     this.showResult = false;
+    this.breakpoint = (window.innerWidth <= 400) ? 1 : 6;
+  }
+
+  onResize(event) {
+    this.breakpoint = (event.target.innerWidth <= 400) ? 1 : 6;
   }
 }
